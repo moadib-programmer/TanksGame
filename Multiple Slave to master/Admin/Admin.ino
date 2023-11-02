ce6e946a12eeb4ddb446b4d6e90e58ca973cd5c0
moadib-programmer <55628036+moadib-programmer@users.noreply.github.com>
2023-11-03 00:32:05 +0500

    Update Admin.ino
    Score update added

--- Multiple Slave to master/Admin/Admin.ino
+++ Multiple Slave to master/Admin/Admin.ino
@@ -4,8 +4,12 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+#define BUTTONPIN       (12U)
+#define NUM_OF_BRAINS   (2U)
+#define VOLT_PIN        (39U)
+#define RED_LED         (34U)
+
 RF24 radio(4, 5); 
-uint8_t startFlag = 0u;
 
 const uint64_t address = 0xF0F0F0F0E1LL;
 
@@ -18,7 +22,18 @@ WiFiServer server(80);
 // Variable to store the HTTP request
 String header;
 // Auxiliary variables to store the current output state
-String StartGame = "off";
+String StartGame = "";
+int startFlag = 0U;
+
+// Define an array of team names and team members
+String teamNames[] = {"Team1", "Team2"};
+String teamMembers[][4] = 
+{
+  {"Team Member 1", "Team Member 2", "Team Member 3", "Team Member 4"},
+  {"Team Member A", "Team Member B", "Team Member C", "Team Member D"},
+};
+int teamScores[NUM_OF_BRAINS] = {0,0};
+String TeamTank[NUM_OF_BRAINS] = {"DAVID BLUE", "NIKE GREEN"};
 
 // Current time
 unsigned long currentTime = millis();
@@ -26,12 +41,6 @@ unsigned long currentTime = millis();
 unsigned long previousTime = 0;
 // Define timeout time in milliseconds (example: 2000ms = 2s)
 const long timeoutTime = 2000;
-
-
-#define BUTTONPIN       (12U)
-#define NUM_OF_BRAINS   (2U)
-#define VOLT_PIN        (39U)
-#define RED_LED         (34U)
 
 int waitForStart();
 
@@ -41,9 +50,6 @@ double volt_measure()
   double voltage = map(volt,0, 3000, 0, 7.4);// map 0-1023 to 0-2500 and add correction offset
   return voltage + 0.5;
 }
-
-String TeamTank[NUM_OF_BRAINS] = {"DAVID BLUE", "NIKE GREEN"};
- 
 
 /************* Structure to send data to Brain *************/
 struct StructureOfTeam
@@ -57,7 +63,7 @@ struct StructureOfTeam
 
 StructureOfTeam TeamData;
  
-/************* Structure to receive data from Admin *************/
+/************* Structure to receive data from Brain *************/
 struct StructureOfBrain
 {
   int counter;
@@ -131,7 +137,6 @@ void setup() {
     }
   } 
 
-  /* TODO: Iterate the ID's here and send Go commands*/
   for(int i = 1; i <= NUM_OF_BRAINS; i++)
   {
     TeamData.go = 1;
@@ -283,30 +288,30 @@ void loop()
  
   Serial.print("Brain ID = ");
   Serial.print(BrainData.brain_id);
+
+  teamScores[BrainData.brain_id - 1] = BrainData.health;
  
   Serial.println();
 
   }
 
-  if (client) 
-  {                             // If a new client connects,
+ if (client) {                             // If a new client connects,
     currentTime = millis();
     previousTime = currentTime;
-    Serial.println("New Client");          // print a message out in the serial port
+    Serial.println("New Client.");          // print a message out in the serial port
     String currentLine = "";                // make a String to hold incoming data from the client
+
+
     while (client.connected() && currentTime - previousTime <= timeoutTime) 
-    { 
+    {  // loop while the client's connected
       currentTime = millis();
       if (client.available()) {             // if there's bytes to read from the client,
         char c = client.read();             // read a byte, then
         Serial.write(c);                    // print it out the serial monitor
         header += c;
-
-        if (c == '\n') 
-        {                    // if the byte is a newline character
+        if (c == '\n') {                    // if the byte is a newline character
           // if the current line is blank, you got two newline characters in a row.
           // that's the end of the client HTTP request, so send a response:
-
           if (currentLine.length() == 0) 
           {
             // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
@@ -315,52 +320,66 @@ void loop()
             client.println("Content-type:text/html");
             client.println("Connection: close");
             client.println();
+            
 
             // turns the GPIOs on and off
-            if (header.indexOf("GET /12/on") >= 0) 
+            if (header.indexOf("GET /GameStart") >= 0) 
             {
               Serial.println("\nGAME STARTED\n");
               StartGame = "on";
               startFlag = 1u;
-            } 
-            else if (header.indexOf("GET /12/off") >= 0) 
-            {
-              StartGame = "off";
+              teamScores[0] = 10;
             } 
 
-  // Display the HTML web page
-client.println("<!DOCTYPE html><html>");
-client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-client.println("<link rel=\"icon\" href=\"data:,\">");
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
 
-// CSS to style the on/off buttons
-// Feel free to change the background-color and font-size attributes to fit your preferences
-client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
-client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-client.println(".square { background-color: #3498db; border-radius: 10px; width: 200px; height: 200px; margin: 20px auto;}");
-client.println("</style></head>");
+            // CSS to style the on/off buttons
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #af4cab; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".rectangle { background-color: #3498db; border-radius: 10px; width: 200px; height: 200px; margin: 20px auto;}");
+            client.println("</style></head>");
 
-// Web Page Heading
-client.println("<body><h1>Tanks Games</h1>");
+            // Web Page Heading
+            client.println("<body><h1>Tanks Games</h1>");
 
-// Start Button
-client.println("<p>Start Button</p>");
-client.println("<p><a href=\"/12/on\"><button class=\"button\">Game Started</button></a></p>");
+            // Start Button
+            client.println("<p>Click to start the game</p>");
+            client.println("<p><a href=\"/GameStart\"><button class=\"button\">Start</button></a></p>");
 
-// Square with Team1 title and random team members
-client.println("<div class=\"square\">");
-client.println("<h2>Team1</h2>");
-client.println("<ul>");
-// Replace these placeholders with your actual team members
-client.println("<li>Team Member 1</li>");
-client.println("<li>Team Member 2</li>");
-client.println("<li>Team Member 3</li>");
-client.println("<li>Team Member 4</li>");
-client.println("</ul>");
-client.println("</div>");
+            // Iterate through the teams and generate squares with scores
+            for (int i = 0; i < 4; i++) 
+            {  // Assuming you have 4 teams
+              client.println("<div class=\"rectangle\" style=\"background-color: #32cd32;\">");
+              client.print("<h2 style=\"color: white;\">");
+              client.print(teamNames[i]);  // Display the team name
+              client.println("</h2>");
+              client.println("<ul>");
 
-client.println("</body></html>");
+              // Generate the list of team members
+              for (int j = 0; j < 4; j++) 
+              {  // Assuming each team has 4 members
+                client.print("<li style=\"color: white; padding-bottom: 5px;\">");
+                client.print(teamMembers[i][j]);  // Display each team member
+                client.println("</li>");
+              }
+
+              // Display the team's score as a styled list item
+              client.print("<li style=\"font-weight: bold; font-size: 22px; color: white;\">");
+              client.print("Score: ");
+              client.print(teamScores[i]);
+              client.println("</li>");
+
+              client.println("</ul>");
+              client.println("</div>");
+            }
+
+
+            client.println("</body></html>");
 
             // The HTTP response ends with another blank line
             client.println();
@@ -386,5 +405,4 @@ client.println("</body></html>");
     Serial.println("Client disconnected.");
     Serial.println("");
   }
-  
 }
