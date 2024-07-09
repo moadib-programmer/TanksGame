6656c68ef3a343741e0d731984398a9e0633482b
moadib-programmer <55628036+moadib-programmer@users.noreply.github.com>
2024-07-09 08:29:44 +0500

    new version of Target implemented

--- Multiple Slave to master/Brain1/Target1B1/Target1B1.ino
+++ Multiple Slave to master/Brain1/Target1B1/Target1B1.ino
@@ -1,31 +1,7 @@
 /*+************* Includes *****************/
-#include <esp_now.h>
-#include <WiFi.h>
-#include <Adafruit_NeoPixel.h>
-#include <esp_wifi.h>
-
-/*+*************** Macros ******************/
-#define PIN               15
-#define NUM               9
-#define VOLT_PIN          34
-#define RED_LED           33
-#define BUTTON_PIN        21
-#define NUMBER_OF_BLINKS  2
-#define GREEN_LED         35
-
-/**
- * @brief HIT Brief
- * If 
- * id = 1, front hit
- * id = 2, side hit
- * id = 3, back hit
- */
-#define TARGET_ID     1
-#define MIN_SCORE     5
-#define MAX_SCORE     10
+#include "target.h"
 
 /*+*************** Globals ******************/
-
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM,PIN, NEO_GRB + NEO_KHZ800);
 volatile float Voltage = 0U;
 // Set your new MAC Address
@@ -144,7 +120,7 @@ void setup()
 
   /* Turning Green LED ON for 2 seconds */
   digitalWrite(GREEN_LED, 1);
-  delay(2000);
+  delay(GREEN_ON_TIME_MS);
   digitalWrite(GREEN_LED, 0);
 
   /* Register Datasend and Datarcv callback functions */
