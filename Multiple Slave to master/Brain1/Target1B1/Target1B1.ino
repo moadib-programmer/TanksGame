ffc361c36982e6c4814a3155c0d6a566b430a76e
moadib-programmer <55628036+moadib-programmer@users.noreply.github.com>
2024-09-03 20:44:31 +0500

    Wireless LED's added[No testing]

--- Multiple Slave to master/Brain1/Target1B1/Target1B1.ino
+++ Multiple Slave to master/Brain1/Target1B1/Target1B1.ino
@@ -2,18 +2,28 @@
 #include "target.h"
 
 /*+*************** Globals ******************/
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_OF_LEDS,PIN, NEO_GRB + NEO_KHZ800);
 volatile float Voltage = 0U;
 // Set your new MAC Address
 uint8_t newMACAddress[] = {0x32, 0xAE, 0xA4, 0x07, 0xB1, 0x01};
 
 uint8_t broadcastAddress[] = {0x42, 0xAE, 0xA4, 0x07, 0x0D, 0x01};
 
+uint8_t targetLedAddress[] = {0x32, 0xAE, 0xA4, 0x07, 0xA1, 0x01};
+
 /******* Structure to send data to the brain ********/
 typedef struct struct_message 
 {
   int id;       // must be unique for each sender board
 } struct_message;
+
+
+/* Variable to be sent to the Target LED 
+ * value = 1 -> HIT (flash RED)
+ * value = 2 -> TURN GREEN
+ * value = 3 -> Turn Yellow
+ * value = 4 -> Turn Red
+ */
+int functionVar = 0;
 
 struct_message myData;
 
@@ -22,20 +32,6 @@ int healthFrmBrain = 0;
 esp_now_peer_info_t peerInfo;
 
 /*+*************** Function definations ******************/
-
-/**
- * @brief voltMeasure: Measures the voltage of the battery
- * 
- * @param None
- * 
- * @return double: measured voltages
-*/
-double voltMeasure()
-{
-  volatile int volt = analogRead(VOLT_PIN);// read the input
-  volatile double voltage = map(volt,0, 2600, 0, 7.4);// map 0-1023 to 0-2500 and add correction offset
-  return voltage + 1U;
-}
 
 /**
  * @brief OnDataSent: callback funcntion when data is sent via ESPNow
@@ -64,29 +60,41 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
 */
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) 
 {
+  esp_err_t transmission_status = ESP_FAIL; 
+
   memcpy(&healthFrmBrain, incomingData, sizeof(healthFrmBrain));
   Serial.print("health Received from the brain: ");
   Serial.println(healthFrmBrain);
-  // delay(DELAY_HIT_FLASH_MS);    
+
+  Serial.println("Sending the command to the Target.");
 
   /* Setting Neopixel depending on healthFrmBrain */
   if( (healthFrmBrain >= 70) and (healthFrmBrain <= 300) )
   {
-    /* Setting neopixel to green, if score is between 70 and 100 */
-    Serial.println("\nUpdated COLOR: Setting the LED to the Green\n");
-    setneopixel(0, 255, 0);
+    /* Setting neopixel to green */
+    Serial.println("\n Sending Green color switching \n");
+    functionVar = 2;
+    transmission_status = esp_now_send(targetLedAddress, (uint8_t *) &functionVar, sizeof(functionVar));
   }
   else if ( (healthFrmBrain >= 40) and (healthFrmBrain <= 69) )
   {
-    /* Setting neopixel to yellow, if score is between 40 and 69 */
-    Serial.println("\nUpdated COLOR: Setting the LED to the Yellow\n");
-    setneopixel(255, 255, 0);
+    /* Setting neopixel to yellow */
+    Serial.println("\n Sending yellow color switching \n");
+    functionVar = 3;
+    transmission_status = esp_now_send(targetLedAddress, (uint8_t *) &functionVar, sizeof(functionVar));
   }
   else if ( (healthFrmBrain >= 0) and (healthFrmBrain <= 39) )
   {
-    /* Setting neopixel to yellow, if score is between 40 and 69 */
-    Serial.println("\nUpdated COLOR: Setting the LED to the Red\n");
-    setneopixel(255, 0, 0);
+    /* Setting neopixel to Red */
+    Serial.println("\n Sending yellow Red switching \n");
+    functionVar = 4;
+    transmission_status = esp_now_send(targetLedAddress, (uint8_t *) &functionVar, sizeof(functionVar));
+  }
+
+  if (transmission_status == ESP_OK) 
+  {
+    Serial.println("Score Sent to the Target LED");
+    delay(500);
   }
 
   Serial.println();
@@ -142,32 +150,41 @@ void setup()
     return;
   }
 
-  Serial.println("******* Setting the LED to GREEN ********");
-  pinMode(BUTTON_PIN, INPUT_PULLDOWN);
+    /* Register peer */
+  memcpy(peerInfo.peer_addr, targetLedAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
   
-  pixels.begin();
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK)
+  {
+    Serial.println("Failed to add peer");
+    return;
+  }
 
-  /* Turning ON GREEN */
-  setneopixel(0, 255, 0);
-  setneopixel(0, 255, 0);
+  Serial.println("******* Initiating the Hit detection ********");
+  pinMode(BUTTON_PIN, INPUT_PULLDOWN);
 }
 
 
 void targetHitCallback()
 {
+  esp_err_t transmission_status = ESP_FAIL;
 
   Serial.println("***** TARGET HIT ****** ");
 
   /* Flashing the Red LED */
-  for (int i = 0; i < NUMBER_OF_BLINKS ; i++)
+  functionVar = 1;
+  transmission_status = esp_now_send(targetLedAddress, (uint8_t *) &functionVar, sizeof(functionVar));
+  
+  if (transmission_status == ESP_OK) 
   {
-    setneopixel(255, 0, 0);
-    delay(RED_FLASH_TIME_MS);
-    setneopixel(0, 0, 0);
-    delay(RED_FLASH_TIME_MS);
+    Serial.println("***** HIT color indication sent ****** ");
   }
 
-    /* Send message via ESP-NOW */
+  delay(1500);
+
+  /* Send message via ESP-NOW */
   Serial.println("*** Sending the Score now ****");
   esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
   
@@ -176,15 +193,6 @@ void targetHitCallback()
     Serial.println("Score Sent to the Brain");
     Serial.println(" ");
     Serial.println("Awaiting Score results ");
-  }
-}
-
-void setneopixel(int r, int g, int b)
-{
-  for(int i = 0; i <= NUM_OF_LEDS; i++)
-  {
-    pixels.setPixelColor(i, pixels.Color(r,g,b));
-    pixels.show();
   }
 }
 
--- Multiple Slave to master/Brain1/Target1B1/target.h
+++ Multiple Slave to master/Brain1/Target1B1/target.h
@@ -1,20 +1,14 @@
 /*+************* Includes *****************/
 #include <esp_now.h>
 #include <WiFi.h>
-#include <Adafruit_NeoPixel.h>
 #include <esp_wifi.h>
 
 /*+*************** Macros ******************/
-#define PIN                15
-#define NUM_OF_LEDS        9
 #define VOLT_PIN           34
 #define RED_LED            33
 #define BUTTON_PIN         21
-#define NUMBER_OF_BLINKS   3
 #define GREEN_LED          35
 #define GREEN_ON_TIME_MS   2000
-#define RED_FLASH_TIME_MS  600
-#define DELAY_HIT_FLASH_MS 2000 
 
 /**
  * @brief HIT Brief
