6ecf8eebe0294c470eb79480a20370ea8132971e
moadib-programmer <55628036+moadib-programmer@users.noreply.github.com>
2024-09-28 14:25:51 +0500

    Audio Test [verified HSPI]

--- Multiple Slave to master/Audio_Test/Audio_Test.ino
+++ Multiple Slave to master/Audio_Test/Audio_Test.ino
@@ -6,38 +6,54 @@
 #include "SD.h"
 #include "FS.h"
 
-// Digital I/O used
-#define SD_CS          5
-#define SPI_MOSI      23    // SD Card
-#define SPI_MISO      19
-#define SPI_SCK       18
 
+
+//hspi pinouts
+#define SD_CS 15
+#define SPI_MOSI 13
+#define SPI_MISO 22
+#define SPI_SCK 14
+
+//speakers pinouts
 #define I2S_DOUT      25
 #define I2S_BCLK      27    // I2S
 #define I2S_LRC       26
 
 Audio audio;
 
-void setup() {
-    pinMode(SD_CS, OUTPUT);      
-    digitalWrite(SD_CS, HIGH);
-    SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+SPIClass *hspi = NULL;
 
-    Serial.begin(115200);
+void setup() 
+{
+  Serial.begin(115200);
+  hspi = new SPIClass(HSPI);
 
-    if(!SD.begin(SD_CS))
-    {
-      Serial.println("Error talking to SD card!");
-      while(true);  // end program
-    }
+  hspi->begin(SPI_SCK, SPI_MISO, SPI_MOSI, SD_CS);
+
+  if(!SD.begin(SD_CS, *hspi))
+  {
+    Serial.println("Error talking to SD card!");
+    while(true);  // end program
+  }
     
-    audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
-    audio.setVolume(15); // 0...21
-    audio.connecttoFS(SD,"/Sample3.mp3");
+  audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
+  audio.setVolume(15); // 0...21
+
+  audio.connecttoFS(SD,"/hit.mp3");
+
+  for(int i = 0; i <= 800; i++)
+  {
+    audio.loop();
+    delay(10);
+  }
 }
 
 void loop()
 {
-    audio.loop();    
+  while(1)
+  {
+    delay(10);
+  }
+
 }
 
